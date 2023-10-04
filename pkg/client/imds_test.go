5eaa98da0117db1e20aba7f07cf75d1e2b4282fe
Cameron Meissner <cameissner@microsoft.com>
2023-10-04 14:18:18 -0700

    feat: cleanup datamodel + add coverage

--- pkg/client/imds_test.go
+++ pkg/client/imds_test.go
@@ -14,9 +14,12 @@ import (
 )
 
 const (
-	mockMSITokenResponseJSON = `{"access_token":"accesstoken","refresh_token":"refreshtoken"}`
-	mockVMSSInstanceDataJSON = `{"compute":{"resourceId": "resourceId"}}`
-	mockVMSSAttestedDataJSON = `{"signature":"signature"}`
+	mockMSITokenResponseJSON          = `{"access_token":"accesstoken"}`
+	mockVMSSInstanceDataJSON          = `{"compute":{"resourceId": "resourceId"}}`
+	mockVMSSAttestedDataJSON          = `{"signature":"signature"}`
+	mockMSITokenResponseJSONWithError = `{"error":"tokenError","error_description":"error generating new JWT"}`
+
+	malformedJSON = `{{}`
 )
 
 func mockImdsWithAssertions(response string, assertions func(r *http.Request)) *httptest.Server {
@@ -104,7 +107,6 @@ var _ = Describe("TLS Bootstrap Client IMDS tests", func() {
 				Expect(err).To(BeNil())
 				Expect(tokenResp).NotTo(BeNil())
 				Expect(tokenResp.AccessToken).To(Equal("accesstoken"))
-				Expect(tokenResp.RefreshToken).To(Equal("refreshtoken"))
 			})
 		})
 
@@ -127,7 +129,48 @@ var _ = Describe("TLS Bootstrap Client IMDS tests", func() {
 				Expect(err).To(BeNil())
 				Expect(tokenResp).NotTo(BeNil())
 				Expect(tokenResp.AccessToken).To(Equal("accesstoken"))
-				Expect(tokenResp.RefreshToken).To(Equal("refreshtoken"))
+			})
+		})
+
+		When("the token response contains an error", func() {
+			It("should return an error with the relevant info", func() {
+				imds := mockImdsWithAssertions(mockMSITokenResponseJSONWithError, func(r *http.Request) {
+					Expect(r.URL.Path).To(Equal("/metadata/identity/oauth2/token"))
+					queryParameters := r.URL.Query()
+					Expect(queryParameters.Get("api-version")).To(Equal("2018-02-01"))
+					Expect(queryParameters.Get("resource")).To(Equal(defaultAKSAADServerAppID))
+					Expect(queryParameters.Has("client_id")).To(BeFalse())
+				})
+				defer imds.Close()
+
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				tokenResp, err := imdsClient.GetMSIToken(ctx, imds.URL, "")
+				Expect(tokenResp).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(ContainSubstring("failed to retrieve MSI token: tokenError: error generating new JWT"))
+			})
+		})
+
+		When("unable to parse token response from IMDS", func() {
+			It("should return an error", func() {
+				imds := mockImdsWithAssertions(malformedJSON, func(r *http.Request) {
+					Expect(r.URL.Path).To(Equal("/metadata/identity/oauth2/token"))
+					queryParameters := r.URL.Query()
+					Expect(queryParameters.Get("api-version")).To(Equal("2018-02-01"))
+					Expect(queryParameters.Get("resource")).To(Equal(defaultAKSAADServerAppID))
+					Expect(queryParameters.Has("client_id")).To(BeFalse())
+				})
+				defer imds.Close()
+
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				tokenResp, err := imdsClient.GetMSIToken(ctx, imds.URL, "")
+				Expect(tokenResp).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(ContainSubstring("failed to unmarshal IMDS data"))
 			})
 		})
 	})
@@ -150,6 +193,26 @@ var _ = Describe("TLS Bootstrap Client IMDS tests", func() {
 			Expect(instanceData).ToNot(BeNil())
 			Expect(instanceData.Compute.ResourceID).To(Equal("resourceId"))
 		})
+
+		When("unable parse instance data response from IMDS", func() {
+			It("should return an error", func() {
+				imds := mockImdsWithAssertions(malformedJSON, func(r *http.Request) {
+					Expect(r.URL.Path).To(Equal("/metadata/instance"))
+					queryParameters := r.URL.Query()
+					Expect(queryParameters.Get("api-version")).To(Equal("2021-05-01"))
+					Expect(queryParameters.Get("format")).To(Equal("json"))
+				})
+				defer imds.Close()
+
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				instanceData, err := imdsClient.GetInstanceData(ctx, imds.URL)
+				Expect(instanceData).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(ContainSubstring("failed to unmarshal IMDS data"))
+			})
+		})
 	})
 
 	Context("GetAttestedData tests", func() {
@@ -171,6 +234,28 @@ var _ = Describe("TLS Bootstrap Client IMDS tests", func() {
 			Expect(err).To(BeNil())
 			Expect(attestedData).ToNot(BeNil())
 			Expect(attestedData.Signature).To(Equal("signature"))
+		})
+
+		When("unable to parse instance data response from IMDS", func() {
+			It("should return an error", func() {
+				imds := mockImdsWithAssertions(malformedJSON, func(r *http.Request) {
+					Expect(r.URL.Path).To(Equal("/metadata/attested/document"))
+					queryParameters := r.URL.Query()
+					Expect(queryParameters.Get("api-version")).To(Equal("2021-05-01"))
+					Expect(queryParameters.Get("format")).To(Equal("json"))
+					Expect(queryParameters.Get("nonce")).To(Equal("nonce"))
+				})
+				defer imds.Close()
+
+				ctx, cancel := context.WithCancel(context.Background())
+				defer cancel()
+
+				nonce := "nonce"
+				attestedData, err := imdsClient.GetAttestedData(ctx, imds.URL, nonce)
+				Expect(attestedData).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(ContainSubstring("failed to unmarshal IMDS data"))
+			})
 		})
 	})
 })
