c6c11d9eacbba1d2788c25bc59859d861d89fa7c
Cameron Meissner <cameissner@microsoft.com>
2023-10-10 11:52:23 -0700

    chore: push changes

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,38 +3,11 @@
 
 package main
 
-import (
-	"context"
-	"flag"
-	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/Azure/aks-tls-bootstrap-client/pkg/client"
-)
+import "os"
 
 func main() {
-	var (
-		clientID    = flag.String("client-id", "", "The client ID for the assigned identity to use.")
-		logFormat   = flag.String("log-format", "json", "Log format: json or text, default: json")
-		nextProto   = flag.String("next-proto", "", "ALPN Next Protocol value to send.")
-		aadResource = flag.String("aad-resource", "", "The resource/audience used to request JWT tokens from AAD")
-		debug       = flag.Bool("debug", false, "enable debug logging (WILL LOG AUTHENTICATION DATA)")
-	)
-
-	flag.Parse()
-	logger := client.GetLogger(*logFormat, *debug)
-	bootstrapClient := client.NewTLSBootstrapClient(logger, *clientID, *nextProto, *aadResource)
-
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer cancel()
-
-	token, err := bootstrapClient.GetBootstrapToken(ctx)
-	if err != nil {
-		logger.Fatalf("Failed to retrieve bootstrap token: %s", err)
+	rootCmd.AddCommand(createBootstrapCommand())
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
 	}
-
-	//nolint:forbidigo // kubelet needs the token printed to stdout
-	fmt.Println(token)
 }
