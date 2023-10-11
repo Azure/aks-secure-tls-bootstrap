1cbb08c42d47330668e7a4219b611f4318bb4d9e
Cameron Meissner <cameissner@microsoft.com>
2023-10-11 08:29:36 -0700

    feat: fix build, add build workflow

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,11 +3,69 @@
 
 package main
 
-import "os"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Azure/aks-tls-bootstrap-client/pkg/client"
+	"github.com/spf13/cobra"
+)
+
+const (
+	flagCustomClientID = "custom-client-id"
+	flagLogFormat      = "log-format"
+	flagNextProto      = "next-proto"
+	flagAADResource    = "aad-resource"
+	flagVerbose        = "verbose"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "tls-bootstrap-client",
+	Short: "tls-bootstrap-client - secure TLS bootstrap client used to generated dynamic TLS bootstrap tokens via the AKS secure TLS bootstrapping protocol",
+}
 
 func main() {
 	rootCmd.AddCommand(createBootstrapCommand())
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func createBootstrapCommand() *cobra.Command {
+	var opts client.SecureTLSBootstrapClientOpts
+
+	cmd := &cobra.Command{
+		Use:   "bootstrap",
+		Short: "generate a secure TLS bootstrap token to securely join an AKS cluster",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+
+			logger := client.GetLogger(opts.LogFormat, opts.Verbose)
+			bootstrapClient := client.NewTLSBootstrapClient(logger, opts)
+
+			ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+			defer cancel()
+
+			bootstrapToken, err := bootstrapClient.GetBootstrapToken(ctx)
+			if err != nil {
+				return err
+			}
+
+			//nolint:forbidigo // kubelet needs the token printed to stdout
+			fmt.Println(bootstrapToken)
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&opts.CustomClientID, flagCustomClientID, "", "The custome user-specified client ID for the assigned identity to use.")
+	cmd.Flags().StringVar(&opts.AADResource, flagAADResource, "", "The resource (audience) used to request JWT tokens from AAD for authentication")
+	cmd.Flags().StringVar(&opts.NextProto, flagNextProto, "", "The ALPN Next Protocol value to send within requests to the bootstrap server.")
+	cmd.Flags().StringVar(&opts.LogFormat, flagLogFormat, "json", "Log format: json or text.")
+	cmd.Flags().BoolVar(&opts.Verbose, flagVerbose, false, "Enable verbose logging.")
+	return cmd
 }
