be7580c1d4cc712fc8dde9636e07f803d61a8755
Cameron Meissner <cameissner@microsoft.com>
2025-05-16 15:45:51 -0700

    refactor(hack/upload): upload as tar archive

--- client/hack/upload.sh
+++ client/hack/upload.sh
@@ -13,7 +13,9 @@ function build_and_upload_linux_amd64() {
     make build OS=linux ARCH=amd64
     client_path="${bin_path}/aks-secure-tls-bootstrap-client-amd64"
     [ ! -f "$client_path" ] && echo "could not find client binary for upload at $client_path" && exit 1
-    az storage blob upload -f "$client_path" --auth-mode login --blob-url "https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/\$web/client/linux/amd64/${VERSION}"
+    tar_path="${bin_path}/linux-amd64.tar.gz"
+    tar -czvf "$tar_path" "$client_path"
+    az storage blob upload -f "$tar_path" --auth-mode login --blob-url "https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/\$web/client/linux/amd64/${VERSION}.tar.gz"
 }
 
 build_and_upload_linux_amd64
