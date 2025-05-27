951deb2f930801bc1268cbcffe35835f90cb40f0
Rafael Mejia <t-ramejia@microsoft.com>
2025-05-27 16:05:37 -0700

    converted CSR tests function to a vanilla go test function

--- client/internal/bootstrap/csr_test.go
+++ client/internal/bootstrap/csr_test.go
@@ -4,12 +4,84 @@
 package bootstrap
 
 import (
+	"testing"
+	"strings"
 	"crypto/x509"
 	"encoding/pem"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	
 )
+
+
+
+func TestMakeKubeletClientCSR(t *testing.T) {
+	
+	csrPEM, keyPEM, err := makeKubeletClientCSR()
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if len(csrPEM) == 0 {
+		t.Errorf("Expected non-empty CSR PEM, got empty")
+	}
+
+	if len(keyPEM) == 0 {
+		t.Errorf("Expected non-empty key PEM, got empty")
+	}
+
+	block, rest := pem.Decode(csrPEM)
+	if len(rest) != 0 {
+		t.Errorf("Expected empty rest, got non-empty")
+	}
+
+	if block == nil {
+		t.Errorf("Expected non-nil block, got nil")
+	}
+
+	csr, err := x509.ParseCertificateRequest(block.Bytes)
+
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	subject := csr.Subject
+	if len(subject.Organization) != 1 {
+		t.Errorf("Expected organization length 1, got %d", len(subject.Organization))
+	}
+
+	if subject.Organization[0] != "system:nodes" {
+		t.Errorf("Expected organization 'system:nodes', got '%s'", subject.Organization[0])
+	}
+
+	if !strings.HasPrefix(subject.CommonName, "system:node:") {
+		t.Errorf("Expected common name to start with 'system:node:', got '%s'", subject.CommonName)
+	}
+
+	if csr.SignatureAlgorithm != x509.ECDSAWithSHA256 {
+		t.Errorf("Expected ECDSAWithSHA256, got %v", csr.SignatureAlgorithm)
+	}
+
+	block, rest = pem.Decode(keyPEM)
+	if len(rest) != 0 {
+		t.Errorf("Expected empty rest, got non-empty %d", len(rest))
+	}
+
+	if block == nil {
+		t.Errorf("Expected non-nil block, got nil")
+	}
+
+	key, err := x509.ParseECPrivateKey(block.Bytes)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	if key == nil {
+		t.Errorf("Expected non-nil key, got nil")
+	}
+
+}
 
 var _ = Describe("CSR tests", func() {
 	Context("makeKubeletClientCSR tests", func() {
