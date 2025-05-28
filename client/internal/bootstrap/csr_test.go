bf913eca18f9656f9b07a5ac2d1bed8097bdfc94
Rafael Mejia <t-ramejia@microsoft.com>
2025-05-28 11:07:46 -0700

    removed ginkgo test

--- client/internal/bootstrap/csr_test.go
+++ client/internal/bootstrap/csr_test.go
@@ -9,10 +9,6 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"github.com/stretchr/testify/assert"
-
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-	
 )
 
 
@@ -46,35 +42,3 @@ func TestMakeKubeletClientCSR(t *testing.T) {
 	assert.NotNil(t, key)
 
 }
-
-var _ = Describe("CSR tests", func() {
-	Context("makeKubeletClientCSR tests", func() {
-		It("should generate and return a new kubelet client CSR for the provided hostname", func() {
-			csrPEM, keyPEM, err := makeKubeletClientCSR()
-			Expect(err).To(BeNil())
-			Expect(csrPEM).ToNot(BeEmpty())
-			Expect(keyPEM).ToNot(BeEmpty())
-
-			block, rest := pem.Decode(csrPEM)
-			Expect(rest).To(BeEmpty())
-			Expect(block).ToNot(BeNil())
-
-			csr, err := x509.ParseCertificateRequest(block.Bytes)
-			Expect(err).To(BeNil())
-
-			subject := csr.Subject
-			Expect(len(subject.Organization)).To(Equal(1))
-			Expect(subject.Organization[0]).To(Equal("system:nodes"))
-			Expect(subject.CommonName).To(HavePrefix("system:node:"))
-			Expect(csr.SignatureAlgorithm).To(Equal(x509.ECDSAWithSHA256))
-
-			block, rest = pem.Decode(keyPEM)
-			Expect(rest).To(BeEmpty())
-			Expect(block).ToNot(BeNil())
-
-			key, err := x509.ParseECPrivateKey(block.Bytes)
-			Expect(err).To(BeNil())
-			Expect(key).ToNot(BeNil())
-		})
-	})
-})
