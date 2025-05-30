643547e8ab7c35935f94a5927809af172ca52895
Rafael Mejia <t-ramejia@microsoft.com>
2025-05-30 15:18:52 -0700

    test: reverted grpc tests

--- client/internal/bootstrap/grpc_test.go
+++ client/internal/bootstrap/grpc_test.go
@@ -8,130 +8,142 @@ import (
 	"os"
 	"path/filepath"
 	"time"
-	"testing"
-	"strings"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 	"google.golang.org/grpc/test/bufconn"
 
 	"github.com/Azure/aks-secure-tls-bootstrap/client/internal/testutil"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestGRPC(t *testing.T) {
+var _ = Describe("grpc", Ordered, func() {
 	var (
 		clusterCACertPEM []byte
-
 	)
-	var err error
-	clusterCACertPEM, _, err = testutil.GenerateCertPEM(testutil.CertTemplate{
-		CommonName:   "hcp",
-		Organization: "aks",
-		IsCA:         true,
-		Expiration:   time.Now().Add(time.Hour),
+
+	BeforeAll(func() {
+		var err error
+		clusterCACertPEM, _, err = testutil.GenerateCertPEM(testutil.CertTemplate{
+			CommonName:   "hcp",
+			Organization: "aks",
+			IsCA:         true,
+			Expiration:   time.Now().Add(time.Hour),
+		})
+		Expect(err).To(BeNil())
 	})
-	assert.NoError(t, err)
-	
-	t.Run("cluster ca data cannot be read", func(t *testing.T) {
-		serviceClient, close, err := getServiceClient("token", &Config{
-			ClusterCAFilePath: "does/not/exist.crt",
-			NextProto:         "nextProto",
-			APIServerFQDN:     "fqdn",
+
+	Context("secureTLSBootstrapServiceClientFactory", func() {
+		When("cluster ca data cannot be read", func() {
+			It("should return an error", func() {
+				serviceClient, close, err := getServiceClient("token", &Config{
+					ClusterCAFilePath: "does/not/exist.crt",
+					NextProto:         "nextProto",
+					APIServerFQDN:     "fqdn",
+				})
+
+				Expect(serviceClient).To(BeNil())
+				Expect(close).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(ContainSubstring("reading cluster CA data from does/not/exist.crt"))
+
+			})
+		})
+		When("cluster ca data is invalid", func() {
+			It("should return an error", func() {
+				tempDir := GinkgoT().TempDir()
+				caFilePath := filepath.Join(tempDir, "ca.crt")
+				err := os.WriteFile(caFilePath, []byte("SGVsbG8gV29ybGQh"), os.ModePerm)
+				Expect(err).To(BeNil())
+
+				serviceClient, close, err := getServiceClient("token", &Config{
+					ClusterCAFilePath: caFilePath,
+					NextProto:         "nextProto",
+					APIServerFQDN:     "fqdn",
+				})
+
+				Expect(serviceClient).To(BeNil())
+				Expect(close).To(BeNil())
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(ContainSubstring("failed to get TLS config"))
+				Expect(err.Error()).To(ContainSubstring("unable to construct new cert pool using cluster CA data"))
+			})
 		})
 
-		assert.Nil(t, serviceClient)
-		assert.Nil(t, close)
-		assert.NotNil(t, err)
-		assert.Equal(t, strings.Contains(err.Error(), "reading cluster CA data from does/not/exist.crt"), true)
-	})
+		When("client connection can be created with provided auth token", func() {
+			It("should create the connection without error", func() {
+				tempDir := GinkgoT().TempDir()
+				caFilePath := filepath.Join(tempDir, "ca.crt")
+				err := os.WriteFile(caFilePath, clusterCACertPEM, os.ModePerm)
+				Expect(err).To(BeNil())
 
-	t.Run("cluster ca data is invalid", func(t *testing.T) {
-		tempDir := t.TempDir()
-		caFilePath := filepath.Join(tempDir, "ca.crt")
-		err := os.WriteFile(caFilePath, []byte("SGVsbG8gV29ybGQh"), os.ModePerm)
-		assert.NoError(t, err)
+				lis := bufconn.Listen(1024)
+				defer lis.Close()
 
-		serviceClient, close, err := getServiceClient("token", &Config{
-			ClusterCAFilePath: caFilePath,
-			NextProto:         "nextProto",
-			APIServerFQDN:     "fqdn",
+				serviceClient, close, err := getServiceClient("token", &Config{
+					ClusterCAFilePath: caFilePath,
+					NextProto:         "nextProto",
+					APIServerFQDN:     lis.Addr().String(),
+				})
+
+				Expect(err).To(BeNil())
+				Expect(close).ToNot(BeNil())
+				Expect(serviceClient).ToNot(BeNil())
+
+				_ = close()
+			})
 		})
-
-		assert.Nil(t, serviceClient)
-		assert.Nil(t, close)
-		assert.NotNil(t, err)
-		assert.Equal(t, strings.Contains(err.Error(), "failed to get TLS config"), true)
-		assert.Equal(t, strings.Contains(err.Error(), "unable to construct new cert pool using cluster CA data"), true)
 	})
 
-	t.Run("client connection can be created with provided auth token", func(t *testing.T) {
-		tempDir := t.TempDir()
-		caFilePath := filepath.Join(tempDir, "ca.crt")
-		err := os.WriteFile(caFilePath, clusterCACertPEM, os.ModePerm)
-		assert.NoError(t, err)
-
-		lis := bufconn.Listen(1024)
-		defer lis.Close()
-
-		serviceClient, close, err := getServiceClient("token", &Config{
-			ClusterCAFilePath: caFilePath,
-			NextProto:         "nextProto",
-			APIServerFQDN:     lis.Addr().String(),
-		})
-
-		assert.NoError(t, err)
-		assert.NotNil(t, close)
-		assert.NotNil(t, serviceClient)
-
-		_ = close()
-	})
-
-	t.Run("getTLSConfig tests", func(t *testing.T) {
+	Context("getTLSConfig tests", func() {
 		var rootPool *x509.CertPool
 
-		rootPool = x509.NewCertPool()
-		ok := rootPool.AppendCertsFromPEM(clusterCACertPEM)
-		assert.True(t, ok)
+		BeforeEach(func() {
+			rootPool = x509.NewCertPool()
+			ok := rootPool.AppendCertsFromPEM(clusterCACertPEM)
+			Expect(ok).To(BeTrue())
+		})
 
-		tests := []struct {
-			name			  string
-			nextProto         string
-			insecureSkipVerify bool
-			expectedNextProtos []string
-		}{
-			{
-				name:              "without nextProto",
-				nextProto:         "",
-				insecureSkipVerify: false,
-				expectedNextProtos: nil,
-			},
-			{
-				name:              "with nextProto",
-				nextProto:         "bootstrap",
-				insecureSkipVerify: false,
-				expectedNextProtos: []string{"bootstrap", "h2"},
-			},
-			{
-				name:              "insecureSkipVerify false",
-				nextProto:         "nextProto",
-				insecureSkipVerify: false,
-				expectedNextProtos: nil,
-			},
-			{
-				name:              "insecureSkipVerify true",
-				nextProto:         "nextProto",
-				insecureSkipVerify: true,
-				expectedNextProtos: nil,
-			},
-		}
-		for _, tt := range tests {
-			t.Run(tt.name, func(t *testing.T) {
-				config, err := getTLSConfig(clusterCACertPEM, tt.nextProto, tt.insecureSkipVerify)
-				assert.NoError(t, err)
-				assert.NotNil(t, config)
-				assert.Equal(t, tt.expectedNextProtos, config.NextProtos)
-				assert.Equal(t, tt.insecureSkipVerify, config.InsecureSkipVerify)
-				assert.True(t, config.RootCAs.Equal(rootPool))
+		When("nextProto is not supplied", func() {
+			It("should not include NextProtos in returned config", func() {
+				config, err := getTLSConfig(clusterCACertPEM, "", false)
+				Expect(err).To(BeNil())
+				Expect(config).ToNot(BeNil())
+				Expect(config.NextProtos).To(BeNil())
+				Expect(config.InsecureSkipVerify).To(BeFalse())
+				Expect(config.RootCAs.Equal(rootPool)).To(BeTrue())
 			})
-		}
+		})
+
+		When("nextProto is supplied", func() {
+			It("should include NextProtos in returned config", func() {
+				config, err := getTLSConfig(clusterCACertPEM, "bootstrap", false)
+				Expect(err).To(BeNil())
+				Expect(config).NotTo(BeNil())
+				Expect(config.NextProtos).NotTo(BeNil())
+				Expect(config.NextProtos).To(Equal([]string{"bootstrap", "h2"}))
+				Expect(config.InsecureSkipVerify).To(BeFalse())
+				Expect(config.RootCAs.Equal(rootPool)).To(BeTrue())
+			})
+		})
+
+		When("insecureSkipVerify is false", func() {
+			It("should return config with false value of InsecureSkipVerify", func() {
+				config, err := getTLSConfig(clusterCACertPEM, "nextProto", false)
+				Expect(err).To(BeNil())
+				Expect(config).NotTo(BeNil())
+				Expect(config.InsecureSkipVerify).To(BeFalse())
+				Expect(config.RootCAs.Equal(rootPool)).To(BeTrue())
+			})
+		})
+
+		When("insecureSkipVerify is true", func() {
+			It("should return config with true value of InsecureSkipVerify", func() {
+				config, err := getTLSConfig(clusterCACertPEM, "nextProto", true)
+				Expect(err).To(BeNil())
+				Expect(config).NotTo(BeNil())
+				Expect(config.InsecureSkipVerify).To(BeTrue())
+				Expect(config.RootCAs.Equal(rootPool)).To(BeTrue())
+			})
+		})
 	})
-}
+})
