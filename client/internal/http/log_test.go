9e43c57336752bb20a621ba15bc8f9c3b8825407
Rafael Mejia <t-ramejia@microsoft.com>
2025-06-10 14:47:25 -0700

    changed test loop to pivot on expected string

--- client/internal/http/log_test.go
+++ client/internal/http/log_test.go
@@ -32,6 +32,42 @@ func TestHttpLog(t *testing.T) {
 		logger    *zap.Logger
 	)
 
+	tests := []struct {
+		name                     string
+		logFunc                  func()
+		expectedStdoutSubstrs    []string
+		notExpectedStdoutSubstrs []string
+	}{
+		{
+			name: "should correctly shim into a zap.Logger",
+			logFunc: func() {
+				shim := &leveledLoggerShim{
+					logger: logger,
+				}
+
+				shim.Info("info", "field", "value")
+				shim.Warn("warn", "field", "value")
+				shim.Error("error", "field", "value")
+				shim.Debug("debug", "field", "value")
+			},
+			notExpectedStdoutSubstrs: []string{"leveled_logger_fields"},
+		},
+		{
+			name: "unexpected number of keys and values are specified",
+			logFunc: func() {
+				shim := &leveledLoggerShim{
+					logger: logger,
+				}
+
+				shim.Info("info", "field", "value", "otherField")
+				shim.Warn("warn", "field", "value", "otherField")
+				shim.Error("error", "field", "value", "otherField")
+				shim.Debug("debug", "field", "value", "otherValue")
+			},
+			expectedStdoutSubstrs: []string{"leveled_logger_fields"},
+		},
+	}
+
 	bufWriter = bufio.NewWriter(&buf)
 
 	config := zap.NewDevelopmentConfig()
@@ -47,38 +83,6 @@ func TestHttpLog(t *testing.T) {
 	logger, err = config.Build()
 	assert.NoError(t, err)
 
-	tests := []struct {
-		name    string
-		logFunc func()
-	}{
-		{
-			name: "should correctly shim into a zap.Logger",
-			logFunc: func() {
-				shim := &leveledLoggerShim{
-					logger: logger,
-				}
-
-				shim.Info("info", "field", "value")
-				shim.Warn("warn", "field", "value")
-				shim.Error("error", "field", "value")
-				shim.Debug("debug", "field", "value")
-			},
-		},
-		{
-			name: "unexpected number of keys and values are specified",
-			logFunc: func() {
-				shim := &leveledLoggerShim{
-					logger: logger,
-				}
-
-				shim.Info("info", "field", "value", "otherField")
-				shim.Warn("warn", "field", "value", "otherField")
-				shim.Error("error", "field", "value", "otherField")
-				shim.Debug("debug", "field", "value", "otherValue")
-			},
-		},
-	}
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.logFunc()
@@ -88,10 +92,13 @@ func TestHttpLog(t *testing.T) {
 
 			out := buf.String()
 			assert.NotEmpty(t, out)
-			if tt.name == "unexpected number of keys and values are specified" {
-				assert.Contains(t, out, "leveled_logger_fields")
-			} else {
-				assert.NotContains(t, out, "leveled_logger_fields")
+
+			for _, substr := range tt.expectedStdoutSubstrs {
+				assert.Contains(t, out, substr)
+			}
+
+			for _, substr := range tt.notExpectedStdoutSubstrs {
+				assert.NotContains(t, out, substr)
 			}
 		})
 	}
