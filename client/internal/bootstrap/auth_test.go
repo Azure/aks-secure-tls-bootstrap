2f081f2eab24d754bc879a127f4deb4a1bfa3ca7
Rafael Mejia <t-ramejia@microsoft.com>
2025-06-11 16:07:01 -0700

    moved assertions to test loop

--- client/internal/bootstrap/auth_test.go
+++ client/internal/bootstrap/auth_test.go
@@ -18,50 +18,34 @@ import (
 )
 
 func TestGetAuthToken(t *testing.T) {
-	var (
-		logger       *zap.Logger
-		testResource = "resource"
-		testTenantID = "d87a2c3e-0c0c-42b2-a883-e48cd8723e22"
-	)
-	logger, _ = zap.NewDevelopment()
+	var testTenantID = "d87a2c3e-0c0c-42b2-a883-e48cd8723e22"
 	tests := []struct {
 		name        string
 		customID    string
-		setupClient func(*testing.T) (*Client, *cloud.ProviderConfig)
-		expectErr   bool
+		setupClient func(*testing.T, *Client) *cloud.ProviderConfig
+		clientStr   string
 		errSubstrs  []string
 		expectToken string
+		expectErr   error
 	}{
 		{
 			name: "error generating MSI access token",
-			setupClient: func(t *testing.T) (*Client, *cloud.ProviderConfig) {
-				client := &Client{
-					logger: logger,
-					extractAccessTokenFunc: func(token *adal.ServicePrincipalToken) (string, error) {
-						assert.NotNil(t, token)
-						return "", errors.New("generating MSI access token")
-					},
-				}
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
 				providerCfg := &cloud.ProviderConfig{
 					UserAssignedIdentityID: "kubelet-identity-id",
 					TenantID:               testTenantID,
 				}
 
-				return client, providerCfg
+				return providerCfg
 			},
-			expectErr:   true,
+			clientStr:   "",
 			errSubstrs:  []string{"generating MSI access token"},
 			expectToken: "",
+			expectErr:   errors.New("generating MSI access token"),
 		},
 		{
 			name: "error getting azure environment config for specified cloud",
-			setupClient: func(t *testing.T) (*Client, *cloud.ProviderConfig) {
-				client := &Client{
-					logger: logger,
-					extractAccessTokenFunc: func(token *adal.ServicePrincipalToken) (string, error) {
-						return "", nil
-					},
-				}
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
 				providerCfg := &cloud.ProviderConfig{
 					CloudName:    "invalid",
 					ClientID:     "service-principal-id",
@@ -69,29 +53,23 @@ func TestGetAuthToken(t *testing.T) {
 					TenantID:     testTenantID,
 				}
 
-				return client, providerCfg
+				return providerCfg
 			},
-			expectErr:   true,
+			clientStr:   "",
 			errSubstrs:  []string{`getting azure environment config for cloud "invalid"`},
 			expectToken: "",
+			expectErr:   nil,
 		},
 		{
 			name: "service principal client secret contains certificate data",
-			setupClient: func(t *testing.T) (*Client, *cloud.ProviderConfig) {
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
 				certData, err := testutil.GenerateCertAndKeyAsEncodedPFXData(testutil.CertTemplate{
 					CommonName:   "aad",
 					Organization: "azure",
 					Expiration:   time.Now().Add(time.Hour),
 				})
 				assert.NoError(t, err)
-				client := &Client{
-					logger: logger,
-					extractAccessTokenFunc: func(token *adal.ServicePrincipalToken) (string, error) {
-						assert.NotNil(t, token)
-						return "token", nil
-					},
-					// certData: certData,
-				}
+
 				providerCfg := &cloud.ProviderConfig{
 					CloudName:    azure.PublicCloud.Name,
 					ClientID:     "service-principal-id",
@@ -99,20 +77,29 @@ func TestGetAuthToken(t *testing.T) {
 					TenantID:     testTenantID,
 				}
 
-				return client, providerCfg
+				return providerCfg
 			},
-			expectErr:   false,
+			clientStr:   "token",
 			errSubstrs:  nil,
 			expectToken: "token",
+			expectErr:   nil,
 		},
 	}
 
+	var testResource = "resource"
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			client, providerCfg := tt.setupClient(t)
+			logger, _ := zap.NewDevelopment()
+			client := &Client{
+				logger: logger,
+				extractAccessTokenFunc: func(token *adal.ServicePrincipalToken) (string, error) {
+					return tt.clientStr, tt.expectErr
+				},
+			}
+			providerCfg := tt.setupClient(t, client)
 			token, err := client.getAccessToken(tt.customID, testResource, providerCfg)
 
-			if tt.expectErr {
+			if len(tt.errSubstrs) > 0 {
 				assert.Error(t, err)
 				for _, substr := range tt.errSubstrs {
 					assert.ErrorContains(t, err, substr)
