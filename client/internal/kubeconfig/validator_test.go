6d8e2e688df42700ba0f73240ccf9d58931c132c
Rafael Mejia <t-ramejia@microsoft.com>
2025-06-11 15:19:54 -0700

    used  expectedError rather than an entire assertions func

--- client/internal/kubeconfig/validator_test.go
+++ client/internal/kubeconfig/validator_test.go
@@ -53,9 +53,9 @@ func TestValidateKubeconfig(t *testing.T) {
 	assert.NoError(t, err)
 
 	tests := []struct {
-		name       string
-		setupFunc  func(v *validator)
-		assertions func(t *testing.T, v *validator)
+		name         string
+		setupFunc    func(v *validator)
+		expectedErrs []string
 	}{
 		{
 			name: "kubeconfig is valid",
@@ -70,10 +70,7 @@ func TestValidateKubeconfig(t *testing.T) {
 					}, nil
 				}
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", false)
-				assert.NoError(t, err)
-			},
+			expectedErrs: []string{},
 		},
 		{
 			name: "the REST config cannot be loaded from the specified kubeconfig",
@@ -82,11 +79,9 @@ func TestValidateKubeconfig(t *testing.T) {
 					return nil, fmt.Errorf("unable to load kubeconfig")
 				}
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", false)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), "failed to create REST client config from kubeconfig")
-				assert.Contains(t, err.Error(), "unable to load kubeconfig")
+			expectedErrs: []string{
+				"failed to create REST client config from kubeconfig",
+				"unable to load kubeconfig",
 			},
 		},
 		{
@@ -96,11 +91,9 @@ func TestValidateKubeconfig(t *testing.T) {
 					return &restclient.Config{}, nil
 				}
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", false)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), "failed to validate client config contents: unable to load TLS certificates from existing kubeconfig")
-				assert.Contains(t, err.Error(), "does not contain any valid RSA or ECDSA certificates")
+			expectedErrs: []string{
+				"failed to validate client config contents: unable to load TLS certificates from existing kubeconfig",
+				"does not contain any valid RSA or ECDSA certificates",
 			},
 		},
 		{
@@ -116,10 +109,8 @@ func TestValidateKubeconfig(t *testing.T) {
 					}, nil
 				}
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", false)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), "private key does not match public key")
+			expectedErrs: []string{
+				"private key does not match public key",
 			},
 		},
 		{
@@ -135,39 +126,37 @@ func TestValidateKubeconfig(t *testing.T) {
 					}, nil
 				}
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", false)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), "some part of the existing kubeconfig certificate has expired")
+			expectedErrs: []string{
+				"some part of the existing kubeconfig certificate has expired",
 			},
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			v := &validator{
 				logger: logger,
 			}
 			tt.setupFunc(v)
-			tt.assertions(t, v)
+			// tt.assertions(t, v)
+			err := v.Validate("path", false)
+			if len(tt.expectedErrs) > 0 {
+				assert.Error(t, err)
+				for _, expectedErr := range tt.expectedErrs {
+					assert.ErrorContains(t, err, expectedErr)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
 
 func TestEnsureAuthorizedClient(t *testing.T) {
-	logger, _ := zap.NewDevelopment()
-	var clientset *fake.Clientset
-
-	validCertPEM, validKeyPEM, err := testutil.GenerateCertPEM(testutil.CertTemplate{
-		CommonName:   "cn",
-		Organization: "org",
-		Expiration:   time.Now().Add(time.Hour),
-	})
-	assert.NoError(t, err)
-
 	tests := []struct {
-		name       string
-		setupFunc  func(v *validator, clientset *fake.Clientset)
-		assertions func(t *testing.T, v *validator)
+		name         string
+		setupFunc    func(v *validator, clientset *fake.Clientset)
+		expectedErrs []string
 	}{
 		{
 			name: "clientset cannot be loaded from client REST config",
@@ -176,11 +165,9 @@ func TestEnsureAuthorizedClient(t *testing.T) {
 					return nil, fmt.Errorf("bad rest config")
 				}
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", true)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), "failed to create clientset from REST client config")
-				assert.Contains(t, err.Error(), "bad rest config")
+			expectedErrs: []string{
+				"failed to create clientset from REST client config",
+				"bad rest config",
 			},
 		},
 		{
@@ -191,11 +178,9 @@ func TestEnsureAuthorizedClient(t *testing.T) {
 						return true, nil, errors.NewUnauthorized("client certificate signed by unknown authority")
 					})
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", true)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), "cannot make authorized request to list server version")
-				assert.Contains(t, err.Error(), "client certificate signed by unknown authority")
+			expectedErrs: []string{
+				"cannot make authorized request to list server version",
+				"client certificate signed by unknown authority",
 			},
 		},
 		{
@@ -206,29 +191,34 @@ func TestEnsureAuthorizedClient(t *testing.T) {
 						return true, nil, errors.NewInternalError(fmt.Errorf("server unavailable"))
 					})
 			},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", true)
-				assert.Error(t, err)
-				assert.Contains(t, err.Error(), "encountered an unexpected error when attempting to request server version info")
-				assert.Contains(t, err.Error(), "server unavailable")
+			expectedErrs: []string{
+				"encountered an unexpected error when attempting to request server version info",
+				"server unavailable",
 			},
 		},
 		{
-			name:      "kubeconfig contains valid cert and key and can make an authorized request to the server",
-			setupFunc: func(v *validator, clientset *fake.Clientset) {},
-			assertions: func(t *testing.T, v *validator) {
-				err := v.Validate("path", true)
-				assert.NoError(t, err)
-			},
+			name:         "kubeconfig contains valid cert and key and can make an authorized request to the server",
+			setupFunc:    func(v *validator, clientset *fake.Clientset) {},
+			expectedErrs: []string{},
 		},
 	}
+
+	validCertPEM, validKeyPEM, err := testutil.GenerateCertPEM(testutil.CertTemplate{
+		CommonName:   "cn",
+		Organization: "org",
+		Expiration:   time.Now().Add(time.Hour),
+	})
+	assert.NoError(t, err)
+
+	logger, _ := zap.NewDevelopment()
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			v := &validator{
 				logger: logger,
 			}
 
-			clientset = fake.NewSimpleClientset()
+			clientset := fake.NewSimpleClientset()
 			v.clientConfigLoader = func(kubeconfigPath string) (*restclient.Config, error) {
 				return &restclient.Config{
 					Host: "https://controlplane.azmk8s.io",
@@ -244,7 +234,16 @@ func TestEnsureAuthorizedClient(t *testing.T) {
 			}
 
 			tt.setupFunc(v, clientset)
-			tt.assertions(t, v)
+			err := v.Validate("path", true)
+
+			if len(tt.expectedErrs) > 0 {
+				assert.Error(t, err)
+				for _, expectedErr := range tt.expectedErrs {
+					assert.ErrorContains(t, err, expectedErr)
+				}
+			} else {
+				assert.NoError(t, err)
+			}
 		})
 	}
 }
