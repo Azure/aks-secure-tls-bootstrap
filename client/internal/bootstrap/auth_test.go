76b72daaf2d732f219de3cd98917058bd53b4637
Rafael Mejia <t-ramejia@microsoft.com>
2025-06-13 17:03:00 -0700

    added back missing tests

--- client/internal/bootstrap/auth_test.go
+++ client/internal/bootstrap/auth_test.go
@@ -58,6 +58,123 @@ func TestGetAuthToken(t *testing.T) {
 			clientStr:   "",
 			errSubstrs:  []string{`getting azure environment config for cloud "invalid"`},
 			expectToken: "",
+			expectErr:   errors.New(`getting azure environment config for cloud "invalid"`),
+		},
+		{
+			name: "there is an error generating a service principal access token with username and password",
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
+				providerCfg := &cloud.ProviderConfig{
+					CloudName:    azure.PublicCloud.Name,
+					ClientID:     "service-principal-id",
+					ClientSecret: "", // empty secret to force a failure
+					TenantID:     testTenantID,
+				}
+				return providerCfg
+			},
+			clientStr:   "",
+			errSubstrs:  []string{"generating SPN access token with username and password"},
+			expectToken: "",
+			expectErr:   errors.New("generating SPN access token with username and password"),
+		},
+		{
+			name: "there is an error b64-decoding the certificate data",
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
+				providerCfg := &cloud.ProviderConfig{
+					CloudName:    azure.PublicCloud.Name,
+					ClientID:     "service-principal-id",
+					ClientSecret: "certificate:YW55IGNhcm5hbCBwbGVhc3U======", // invalid b64-encoding
+					TenantID:     testTenantID,
+				}
+				return providerCfg
+			},
+			clientStr:   "",
+			errSubstrs:  []string{"b64-decoding certificate data in client secret"},
+			expectToken: "",
+			expectErr:   errors.New("b64-decoding certificate data in client secret"),
+		},
+		{
+			name: "there is an error decoding the pfx certificate data",
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
+				providerCfg := &cloud.ProviderConfig{
+					CloudName:    azure.PublicCloud.Name,
+					ClientID:     "service-principal-id",
+					ClientSecret: "certificate:dGVzdAo=", // b64-encoding of "test"
+					TenantID:     testTenantID,
+				}
+				return providerCfg
+			},
+			clientStr:   "",
+			errSubstrs:  []string{"decoding pfx certificate data in client secret"},
+			expectToken: "",
+			expectErr:   errors.New("decoding pfx certificate data in client secret"),
+		},
+		{
+			name: "there is an error generating a service principal token with certificate data",
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
+				certData, err := testutil.GenerateCertAndKeyAsEncodedPFXData(testutil.CertTemplate{
+					CommonName:   "aad",
+					Organization: "azure",
+					Expiration:   time.Now().Add(time.Hour),
+				})
+				assert.NoError(t, err)
+
+				providerCfg := &cloud.ProviderConfig{
+					CloudName:    azure.PublicCloud.Name,
+					ClientID:     "service-principal-id",
+					ClientSecret: "certificate:" + certData,
+					TenantID:     testTenantID,
+				}
+
+				return providerCfg
+			},
+			clientStr:   "",
+			errSubstrs:  []string{"generating SPN access token with certificate"},
+			expectToken: "",
+			expectErr:   errors.New("generating SPN access token with certificate"),
+		},
+		{
+			name: "UserAssignedIdentityID is specified in cloud provider config",
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
+				providerCfg := &cloud.ProviderConfig{
+					UserAssignedIdentityID: "kubelet-identity-id",
+					TenantID:               testTenantID,
+				}
+				return providerCfg
+			},
+			clientStr:   "token",
+			errSubstrs:  nil,
+			expectToken: "token",
+			expectErr:   nil,
+		},
+		{
+			name:     "a custom client ID is specified",
+			customID: "custom",
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
+				providerCfg := &cloud.ProviderConfig{
+					UserAssignedIdentityID: "kubelet-identity-id",
+					TenantID:               testTenantID,
+				}
+				return providerCfg
+			},
+			clientStr:   "token",
+			errSubstrs:  nil,
+			expectToken: "token",
+			expectErr:   nil,
+		},
+		{
+			name: "service principal client secret does not contain certificate data",
+			setupClient: func(t *testing.T, client *Client) *cloud.ProviderConfig {
+				cloudProviderConfig := &cloud.ProviderConfig{
+					CloudName:    azure.PublicCloud.Name,
+					ClientID:     "service-principal-id",
+					ClientSecret: "secret",
+					TenantID:     testTenantID,
+				}
+				return cloudProviderConfig
+			},
+			clientStr:   "token",
+			errSubstrs:  nil,
+			expectToken: "token",
 			expectErr:   nil,
 		},
 		{
